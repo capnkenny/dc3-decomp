ed794117de3d454cebdf5bfbffae78e48eaea668
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-03-15 12:50:48 -0700

    fix objptrlist propsync

--- src/system/rndobj/ScreenMask.cpp
+++ src/system/rndobj/ScreenMask.cpp
@@ -1,18 +1,32 @@
 #include "rndobj/ScreenMask.h"
+#include "obj/Object.h"
 #include "rndobj/Draw.h"
 #include "utl/BinStream.h"
 
-void RndScreenMask::Save(BinStream &bs) {
-    bs << 2;
-    SAVE_SUPERCLASS(Hmx::Object)
-    SAVE_SUPERCLASS(RndDrawable)
-    bs << mMat << mColor << mRect << mUseCamRect;
-}
+RndScreenMask::RndScreenMask()
+    : mMat(this), mColor(1, 1, 1, 1), mRect(0, 0, 1, 1), mUseCamRect(false) {}
 
 BEGIN_HANDLERS(RndScreenMask)
     HANDLE_SUPERCLASS(RndDrawable)
     HANDLE_SUPERCLASS(Hmx::Object)
 END_HANDLERS
+
+BEGIN_PROPSYNCS(RndScreenMask)
+    SYNC_PROP(mat, mMat)
+    SYNC_PROP(color, mColor)
+    SYNC_PROP(alpha, mColor.alpha)
+    SYNC_PROP(screen_rect, mRect)
+    SYNC_PROP(use_cam_rect, mUseCamRect)
+    SYNC_SUPERCLASS(RndDrawable)
+    SYNC_SUPERCLASS(Hmx::Object)
+END_PROPSYNCS
+
+BEGIN_SAVES(RndScreenMask)
+    SAVE_REVS(2, 0)
+    SAVE_SUPERCLASS(Hmx::Object)
+    SAVE_SUPERCLASS(RndDrawable)
+    bs << mMat << mColor << mRect << mUseCamRect;
+END_SAVES
 
 BEGIN_COPYS(RndScreenMask)
     COPY_SUPERCLASS(Hmx::Object)
@@ -26,15 +40,19 @@ BEGIN_COPYS(RndScreenMask)
     END_COPYING_MEMBERS
 END_COPYS
 
-BEGIN_PROPSYNCS(RndScreenMask)
-    SYNC_PROP(mat, mMat)
-    SYNC_PROP(color, mColor)
-    SYNC_PROP(alpha, mColor.alpha)
-    SYNC_PROP(screen_rect, mRect)
-    SYNC_PROP(use_cam_rect, mUseCamRect)
-    SYNC_SUPERCLASS(RndDrawable)
-    SYNC_SUPERCLASS(Hmx::Object)
-END_PROPSYNCS
+INIT_REVS(2, 0)
 
-RndScreenMask::RndScreenMask()
-    : mMat(this), mColor(1, 1, 1, 1), mRect(0, 0, 1, 1), mUseCamRect(false) {}
+BEGIN_LOADS(RndScreenMask)
+    LOAD_REVS(bs)
+    ASSERT_REVS(2, 0)
+    LOAD_SUPERCLASS(Hmx::Object)
+    LOAD_SUPERCLASS(RndDrawable)
+    d >> mMat;
+    d >> mColor;
+    if (d.rev > 0) {
+        d >> mRect;
+    }
+    if (d.rev > 1) {
+        d >> mUseCamRect;
+    }
+END_LOADS
