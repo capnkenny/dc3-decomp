f3cb1e382e1b2a578d7d838348ad7c4276cba958
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-03-17 21:20:25 -0700

    match some big rndmat funcs

--- src/system/rndobj/Mat.cpp
+++ src/system/rndobj/Mat.cpp
@@ -82,22 +82,40 @@ END_HANDLERS
             if (!(_op & (kPropSize | kPropGet)) && !IsEditable(action)) {                \
                 return true;                                                             \
             }                                                                            \
-            if (!PropSync(member, _val, _prop, _i + 1, _op))                             \
+            if (PropSync(member, _val, _prop, _i + 1, _op)) {                            \
+                if (!(_op & (kPropSize | kPropGet))) {                                   \
+                    mDirty |= dirty_flag;                                                \
+                }                                                                        \
+                return true;                                                             \
+            } else                                                                       \
                 return false;                                                            \
-            if (!(_op & (kPropSize | kPropGet)))                                         \
-                mDirty |= dirty_flag;                                                    \
+        }                                                                                \
+    }
+
+#define SYNC_PERF_PROP(s, member)                                                        \
+    {                                                                                    \
+        _NEW_STATIC_SYMBOL(s)                                                            \
+        if (sym == _s) {                                                                 \
+            if (_op == kPropSet) {                                                       \
+                Symbol action(#s "_edit_action");                                        \
+                if (IsEditable(action)) {                                                \
+                    member = _val.Int() > 0;                                             \
+                }                                                                        \
+            } else {                                                                     \
+                if (_op == (PropOp)0x40)                                                 \
+                    return false;                                                        \
+                _val = member;                                                           \
+            }                                                                            \
             return true;                                                                 \
         }                                                                                \
     }
 
 BEGIN_PROPSYNCS(RndMat)
-    // clang-format off
-    SYNC_PROP_SET(metamaterial, mMetaMaterial.Ptr(),
-        mMetaMaterial = _val.Obj<MetaMaterial>();
+    SYNC_PROP_SET(
+        metamaterial, mMetaMaterial.Ptr(), mMetaMaterial = _val.Obj<MetaMaterial>();
         UpdatePropertiesFromMetaMat();
         unk225 = false;
     )
-    // clang-format on
     SYNC_MAT_PROP(intensify, mIntensify, 2)
     SYNC_MAT_PROP(blend, (int &)mBlend, 2)
     SYNC_MAT_PROP(color, mColor, 1)
@@ -112,24 +130,46 @@ BEGIN_PROPSYNCS(RndMat)
     SYNC_MAT_PROP(diffuse_tex2, mDiffuseTex2, 2)
     SYNC_MAT_PROP(prelit, mPrelit, 2)
     SYNC_MAT_PROP(alpha_cut, mAlphaCut, 2)
-    SYNC_PROP(alpha_threshold, mAlphaThreshold)
-    SYNC_MAT_PROP(alpha_write, mAlphaWrite, 2)
-    SYNC_PROP(force_alpha_write, mForceAlphaWrite)
-    SYNC_MAT_PROP(next_pass, mNextPass, 2)
+    SYNC_PROP_MODIFY(alpha_threshold, mAlphaThreshold, mDirty |= 2)
+    SYNC_MAT_PROP(alpha_write, mAlphaWrite, 2) {
+        static Symbol _s("force_alpha_write");
+        if (sym == _s)
+            return PropSync(mForceAlphaWrite, _val, _prop, _i + 1, _op) != false;
+    }
+
+    {
+        static Symbol _s("next_pass");
+        if (sym == _s) {
+            Symbol action("next_pass_edit_action");
+            if (!(_op & (kPropSize | kPropGet)) && !IsEditable(action)) {
+                return true;
+            }
+            return PropSync(mNextPass, _val, _prop, _i + 1, _op) != false;
+        }
+    }
     SYNC_MAT_PROP(cull, (int &)mCull, 2)
     SYNC_MAT_PROP(per_pixel_lit, mPerPixelLit, 2)
     SYNC_MAT_PROP(emissive_multiplier, mEmissiveMultiplier, 2)
-    SYNC_MAT_PROP(specular_rgb, mSpecularRGB, 2)
-    SYNC_MAT_PROP(specular_power, mSpecularRGB.alpha, 2)
-    SYNC_MAT_PROP(specular2_rgb, mSpecular2RGB, 2)
-    SYNC_MAT_PROP(specular2_power, mSpecular2RGB.alpha, 2)
+    SYNC_MAT_PROP(specular_rgb, mSpecularRGB, 1)
+    SYNC_MAT_PROP(specular_power, mSpecularRGB.alpha, 1)
+    SYNC_MAT_PROP(specular2_rgb, mSpecular2RGB, 1)
+    SYNC_MAT_PROP(specular2_power, mSpecular2RGB.alpha, 1)
     SYNC_MAT_PROP(normal_map, mNormalMap, 2)
-    SYNC_MAT_PROP(emissive_map, mEmissiveMap, 2)
-    SYNC_PROP_SET(
-        specular_map,
-        mSpecularMap.Ptr(),
-        if (IsEditable("specular_map_edit_action")) SetSpecularMap(_val.Obj<RndTex>())
-    )
+    SYNC_MAT_PROP(emissive_map, mEmissiveMap, 2) {
+        static Symbol _s("specular_map");
+        if (sym == _s) {
+            if (_op == kPropSet) {
+                Symbol action("specular_map_edit_action");
+                if (IsEditable(action))
+                    SetSpecularMap(_val.Obj<RndTex>());
+            } else {
+                if (_op == (PropOp)0x40)
+                    return false;
+                _val = mSpecularMap.Ptr();
+            }
+            return true;
+        }
+    }
     SYNC_MAT_PROP(environ_map, mEnvironMap, 2)
     SYNC_MAT_PROP(environ_map_falloff, mEnvironMapFalloff, 2)
     SYNC_MAT_PROP(environ_map_specmask, mEnvironMapSpecMask, 2)
@@ -146,26 +186,59 @@ BEGIN_PROPSYNCS(RndMat)
     SYNC_MAT_PROP(refract_strength, mRefractStrength, 2)
     SYNC_MAT_PROP(refract_normal_map, mRefractNormalMap, 2)
     SYNC_MAT_PROP(screen_aligned, mScreenAligned, 2)
-    SYNC_MAT_PROP(shader_variation, (int &)mShaderVariation, 2)
-    SYNC_MAT_PROP(point_lights, mPointLights, 2)
-    SYNC_MAT_PROP(fog, mFog, 2)
-    SYNC_MAT_PROP(fade_out, mFadeout, 2)
-    SYNC_MAT_PROP(color_adjust, mColorAdjust, 2)
-    SYNC_MAT_PROP(fur, mFur, 2)
-    // clang-format off
-    SYNC_PROP_SET(recv_proj_lights, mPerfSettings.mRecvProjLights,
-        if (IsEditable("recv_proj_lights_edit_action"))
-            mPerfSettings.mRecvProjLights = _val.Int() > 0;
-    )
-    SYNC_PROP_SET(recv_point_cube_tex, mPerfSettings.mRecvPointCubeTex,
-        if (IsEditable("recv_point_cube_tex_edit_action"))
-            mPerfSettings.mRecvPointCubeTex = _val.Int() > 0;
-    )
-    SYNC_PROP_SET(ps3_force_trilinear, mPerfSettings.mPS3ForceTrilinear,
-        if (IsEditable("ps3_force_trilinear_edit_action"))
-            mPerfSettings.mPS3ForceTrilinear = _val.Int() > 0;
-    )
-    // clang-format on
+    SYNC_MAT_PROP(shader_variation, (int &)mShaderVariation, 2) {
+        static Symbol _s("point_lights");
+        if (sym == _s) {
+            Symbol action("point_lights_edit_action");
+            if (!(_op & (kPropSize | kPropGet)) && !IsEditable(action)) {
+                return true;
+            }
+            return PropSync(mPointLights, _val, _prop, _i + 1, _op) != false;
+        }
+    }
+    {
+        static Symbol _s("fog");
+        if (sym == _s) {
+            Symbol action("fog_edit_action");
+            if (!(_op & (kPropSize | kPropGet)) && !IsEditable(action)) {
+                return true;
+            }
+            return PropSync(mFog, _val, _prop, _i + 1, _op) != false;
+        }
+    }
+    {
+        static Symbol _s("fade_out");
+        if (sym == _s) {
+            Symbol action("fade_out_edit_action");
+            if (!(_op & (kPropSize | kPropGet)) && !IsEditable(action)) {
+                return true;
+            }
+            return PropSync(mFadeout, _val, _prop, _i + 1, _op) != false;
+        }
+    }
+    {
+        static Symbol _s("color_adjust");
+        if (sym == _s) {
+            Symbol action("color_adjust_edit_action");
+            if (!(_op & (kPropSize | kPropGet)) && !IsEditable(action)) {
+                return true;
+            }
+            return PropSync(mColorAdjust, _val, _prop, _i + 1, _op) != false;
+        }
+    }
+    {
+        static Symbol _s("fur");
+        if (sym == _s) {
+            Symbol action("fur_edit_action");
+            if (!(_op & (kPropSize | kPropGet)) && !IsEditable(action)) {
+                return true;
+            }
+            return PropSync(mFur, _val, _prop, _i + 1, _op) != false;
+        }
+    }
+    SYNC_PERF_PROP(recv_proj_lights, mPerfSettings.mRecvProjLights)
+    SYNC_PERF_PROP(recv_point_cube_tex, mPerfSettings.mRecvPointCubeTex)
+    SYNC_PERF_PROP(ps3_force_trilinear, mPerfSettings.mPS3ForceTrilinear)
     SYNC_MAT_PROP(bloom_multiplier, mBloomMultiplier, 2)
     SYNC_MAT_PROP(never_fit_to_spline, mNeverFitToSpline, 2)
     SYNC_MAT_PROP(allow_distortion_effects, mAllowDistortionEffects, 2)
@@ -202,13 +275,14 @@ INIT_REVS(0x46, 0)
 BEGIN_LOADS(RndMat)
     LOAD_REVS(bs)
     ASSERT_REVS(0x46, 0)
+    int maxOld = 0x19;
     MILO_ASSERT_FMT(
-        d.rev >= 0x19,
+        d.rev >= maxOld,
         "%s can't load old %s version %d < %d.  Use RB2 Milo to load.",
         PathName(this),
         ClassName(),
         d.rev,
-        0x19
+        maxOld
     );
     mDirty = 3;
     ResetColors(mColorMod, 3);
@@ -259,9 +333,7 @@ RndTex *RndMat::GetRefractNormalMap() {
 bool RndMat::GetRefractEnabled(bool b1) {
     if (mRefractEnabled == 1 && mRefractStrength > 0.0f) {
         RndTex *tex = mRefractNormalMap ? mRefractNormalMap : mNormalMap;
-        if (tex && (b1 || TheRnd.GetCurrentFrameTex(false))) {
-            return true;
-        }
+        return tex && (b1 || TheRnd.GetCurrentFrameTex(false));
     }
     return false;
 }
@@ -469,7 +541,7 @@ void RndMat::UpdatePropertiesFromMetaMat() {
         unk226 = true;
         String str70;
         std::list<Symbol> syms78;
-        ListProperties(syms78, 0, "Mat", nullptr, false);
+        ListProperties(syms78, "Mat", 0, nullptr, false);
         for (std::list<Symbol>::iterator it = syms78.begin(); it != syms78.end(); ++it) {
             static Symbol metamaterial("metamaterial");
             Symbol cur = *it;
@@ -495,65 +567,61 @@ void RndMat::UpdatePropertiesFromMetaMat() {
     mDirty |= 2;
 }
 
-void RndMat::LoadOld(BinStreamRev &bs) {
-    Hmx::Object::Load(bs.stream);
-    bs >> (int &)mBlend;
+void RndMat::LoadOld(BinStreamRev &d) {
+    Hmx::Object::Load(d.stream);
+    d >> (int &)mBlend;
     mBlend = CheckBlendMode(mBlend, this);
-    bs >> mColor;
-    bs >> mUseEnviron >> mPrelit;
-    bs >> (int &)mZMode;
-    bs >> mAlphaCut;
-    if (bs.rev > 0x25) {
-        bs >> mAlphaThreshold;
+    d >> mColor;
+    d >> mUseEnviron >> mPrelit;
+    d >> (int &)mZMode;
+    d >> mAlphaCut;
+    if (d.rev > 0x25) {
+        d >> mAlphaThreshold;
     }
-    bs >> mAlphaWrite;
-    bs >> (int &)mTexGen;
-    bs >> (int &)mTexWrap;
-    bs >> mTexXfm;
-    bs >> mDiffuseTex;
-    bs >> mNextPass;
-    bs.stream >> mIntensify;
+    d >> mAlphaWrite;
+    d >> (int &)mTexGen;
+    d >> (int &)mTexWrap;
+    d >> mTexXfm >> mDiffuseTex;
+    d.stream >> mNextPass >> mIntensify;
     bool bCull;
-    bs >> bCull;
-    mCull = (Cull)bCull;
-    bs >> mEmissiveMultiplier;
-    bs >> mSpecularRGB;
-    bs >> mNormalMap;
-    bs >> mEmissiveMap;
-    bs >> mSpecularMap;
-    if (bs.rev < 0x33) {
+    d >> bCull;
+    mCull = (Cull)(bCull != 0);
+    d >> mEmissiveMultiplier;
+    d.stream >> mSpecularRGB >> mNormalMap;
+    d.stream >> mEmissiveMap >> mSpecularMap;
+    if (d.rev < 0x33) {
         ObjPtr<RndTex> tex(this);
-        bs >> tex;
+        d >> tex;
     }
-    bs >> mEnvironMap;
-    if (bs.rev > 0x3C) {
-        bs >> mEnvironMapFalloff;
-        if (bs.rev > 0x42) {
-            bs >> mEnvironMapSpecMask;
+    d >> mEnvironMap;
+    if (d.rev > 0x3C) {
+        d >> mEnvironMapFalloff;
+        if (d.rev > 0x42) {
+            d >> mEnvironMapSpecMask;
         }
     }
-    if (bs.rev < 0x25) {
+    if (d.rev < 0x25) {
         if (mSpecularMap) {
-            mSpecularRGB.Set(1, 1, 1);
+            mSpecularRGB.Set(1, 1, 1, mSpecularRGB.alpha);
         }
     }
-    if (bs.rev > 0x19) {
-        bs >> mPerPixelLit;
+    if (d.rev > 0x19) {
+        d >> mPerPixelLit;
     }
-    if (bs.rev > 0x1A && bs.rev < 0x32) {
+    if (d.rev > 0x1A && d.rev < 0x32) {
         bool b150;
-        bs >> b150;
+        d >> b150;
     }
-    if (bs.rev > 0x1B) {
-        bs >> (int &)mStencilMode;
+    if (d.rev > 0x1B) {
+        d >> (int &)mStencilMode;
     }
-    if (bs.rev < 0x29 && bs.rev > 0x1C) {
+    if (d.rev < 0x29 && d.rev > 0x1C) {
         Symbol s;
-        bs >> s;
+        d >> s;
     }
-    if (bs.rev > 0x20) {
-        bs >> mFur;
-    } else if (bs.rev > 0x1D) {
+    if (d.rev > 0x20) {
+        d >> mFur;
+    } else if (d.rev > 0x1D) {
         bool old = TheLoadMgr.EditMode();
         TheLoadMgr.SetEditMode(true);
         const char *name = MakeString("%s.fur", FileGetBase(Name()));
@@ -563,77 +631,77 @@ void RndMat::LoadOld(BinStreamRev &bs) {
             fur->SetName(name, dir);
         }
         TheLoadMgr.SetEditMode(old);
-        if (fur->LoadOld(bs)) {
+        if (fur->LoadOld(d)) {
             mFur = fur;
         } else {
             delete fur;
             mFur = nullptr;
         }
     }
-    if (bs.rev > 0x21 && bs.rev < 0x31) {
+    if (d.rev > 0x21 && d.rev < 0x31) {
         bool b150;
         Hmx::Color c80;
-        bs >> b150 >> c80;
-        if (bs.rev > 0x22) {
+        d >> b150 >> c80;
+        if (d.rev > 0x22) {
             ObjPtr<RndTex> tex(this);
-            bs >> tex;
+            d >> tex;
         }
     }
-    if (bs.rev > 0x23) {
-        bs >> mDeNormal;
-        bs >> mAnisotropy;
+    if (d.rev > 0x23) {
+        d >> mDeNormal;
+        d >> mAnisotropy;
     }
-    if (bs.rev > 0x26) {
-        if (bs.rev < 0x2A) {
+    if (d.rev > 0x26) {
+        if (d.rev < 0x2A) {
             bool b150;
-            bs >> b150;
+            d >> b150;
         }
-        bs >> mNormDetailTiling;
-        bs >> mNormDetailStrength;
-        if (bs.rev < 0x2A) {
+        d >> mNormDetailTiling;
+        d >> mNormDetailStrength;
+        if (d.rev < 0x2A) {
             int x;
             Hmx::Color c;
-            bs >> x;
-            bs >> c;
+            d >> x;
+            d >> c;
         }
-        bs >> mNormDetailMap;
-        if (bs.rev < 0x2A) {
+        d >> mNormDetailMap;
+        if (d.rev < 0x2A) {
             ObjPtr<RndTex> tex(this);
-            bs >> tex;
+            d >> tex;
         }
-        if (bs.rev < 0x28) {
+        if (d.rev < 0x28) {
             mNormDetailStrength = 0;
         }
     }
-    if (bs.rev > 0x2A) {
-        if (bs.rev > 0x2C) {
-            bs >> mPointLights;
+    if (d.rev > 0x2A) {
+        if (d.rev > 0x2C) {
+            d >> mPointLights;
         } else {
             int x;
-            bs >> x;
-            mPointLights = x == 1;
+            d >> x;
+            mPointLights = x > 1;
         }
-        if (bs.rev < 0x3F) {
+        if (d.rev < 0x3F) {
             bool b150;
-            bs >> b150;
+            d >> b150;
         }
-        bs >> mFog >> mFadeout;
-        if (bs.rev > 0x2B && bs.rev < 0x2E) {
+        d >> mFog >> mFadeout;
+        if (d.rev > 0x2B && d.rev < 0x2E) {
             bool b150;
-            bs >> b150;
+            d >> b150;
         }
-        if (bs.rev > 0x2E) {
-            bs >> mColorAdjust;
+        if (d.rev > 0x2E) {
+            d >> mColorAdjust;
         }
     }
-    if (bs.rev > 0x2F) {
-        bs >> mRimRGB;
-        bs >> mRimMap;
-        if (bs.rev >= 0x3A) {
-            bs >> mRimLightUnder;
+    if (d.rev > 0x2F) {
+        d >> mRimRGB;
+        d >> mRimMap;
+        if (d.rev > 0x39) {
+            d >> mRimLightUnder;
         } else {
             bool b150;
-            bs >> b150;
+            d >> b150;
             float red = mRimRGB.red * 2.857143f;
             float green = mRimRGB.green * 2.857143f;
             float blue = mRimRGB.blue * 2.857143f;
@@ -641,62 +709,64 @@ void RndMat::LoadOld(BinStreamRev &bs) {
             mRimRGB.green = Min(green, 1.0f);
             mRimRGB.blue = Min(blue, 1.0f);
         }
-        if (bs.rev < 0x3B) {
-            mRimRGB.Set(0, 0, 0);
+        if (d.rev < 0x3B) {
+            mRimRGB.red = 0;
+            mRimRGB.green = 0;
+            mRimRGB.blue = 0;
         }
     }
-    if (bs.rev > 0x30) {
-        bs >> mScreenAligned;
+    if (d.rev > 0x30) {
+        d >> mScreenAligned;
     }
-    if (bs.rev > 0x31 && bs.rev < 0x33) {
+    if (d.rev > 0x31 && d.rev < 0x33) {
         bool b150;
-        bs >> b150;
+        d >> b150;
         if (b150) {
             mShaderVariation = kShaderVariationSkin;
         }
     }
-    if (bs.rev > 0x32) {
-        bs >> (int &)mShaderVariation;
-        bs >> mSpecular2RGB;
+    if (d.rev > 0x32) {
+        d >> (int &)mShaderVariation;
+        d >> mSpecular2RGB;
     }
-    if (bs.rev > 0x33 && bs.rev < 0x44) {
+    if (d.rev > 0x33 && d.rev < 0x44) {
         std::vector<Hmx::Color> colors;
-        if (bs.rev < 0x35) {
+        if (d.rev < 0x35) {
             bool x;
-            bs >> x;
+            d >> x;
         } else {
             int x;
-            bs >> x;
+            d >> x;
         }
-        if (bs.rev > 0x34 && bs.rev < 0x3C) {
+        if (d.rev > 0x34 && d.rev < 0x3C) {
             Hmx::Color c;
-            bs >> c;
+            d >> c;
         }
-        if (bs.rev >= 0x3C) {
-            bs >> colors;
+        if (d.rev >= 0x3C) {
+            d >> colors;
         }
     }
-    if (bs.rev > 0x35 && bs.rev < 0x3E) {
+    if (d.rev > 0x35 && d.rev < 0x3E) {
         ObjPtr<Hmx::Object> obj(this);
-        bs >> obj;
+        d >> obj;
     }
-    if (bs.rev > 0x36 && bs.rev < 0x3F) {
+    if (d.rev > 0x36 && d.rev < 0x3F) {
         bool b150;
-        bs >> b150;
+        d >> b150;
         mPerfSettings.mPS3ForceTrilinear = b150;
     }
-    if (bs.rev > 0x37 && bs.rev < 0x39) {
+    if (d.rev > 0x37 && d.rev < 0x39) {
         int x, y;
-        bs >> x >> y;
+        d >> x >> y;
     }
-    if (bs.rev > 0x3E) {
-        mPerfSettings.LoadOld(bs);
+    if (d.rev > 0x3E) {
+        mPerfSettings.LoadOld(d);
     }
-    if (bs.rev > 0x3F) {
-        bs >> mRefractEnabled;
-        bs >> mRefractStrength;
-        bs >> mRefractNormalMap;
-        if (bs.rev < 0x41) {
+    if (d.rev > 0x3F) {
+        d >> mRefractEnabled;
+        d >> mRefractStrength;
+        d >> mRefractNormalMap;
+        if (d.rev < 0x41) {
             if (mRefractEnabled) {
                 mRefractStrength *= 0.15f;
             } else {
