20dbbf2ed1815bb7b51b5a2a91215c661385feea
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-05-09 12:00:24 -0700

    more clipdistmap

--- src/system/char/ClipDistMap.cpp
+++ src/system/char/ClipDistMap.cpp
@@ -1,6 +1,7 @@
 #include "char/ClipDistMap.h"
 #include "char/CharClip.h"
 #include "math/Utl.h"
+#include "os/Debug.h"
 #include "rndobj/Rnd.h"
 #include "utl/Std.h"
 #include <cmath>
@@ -78,59 +79,59 @@ bool ClipDistMap::BeatAligned(int i1, int i2) {
 }
 
 bool ClipDistMap::FindBestNode(float f1, float f2, float f3, ClipDistMap::Node &node) {
-    int temp1;
-    int temp2;
-    if (f2 < f3) {
-        temp1 = (f3 - mAStart) * mSamplesPerBeat;
-        temp2 = (f2 - mAStart) * mSamplesPerBeat;
-        temp2 = 0xffffffff - (temp2 >> 0x1f) & temp2;
-        int max = Max(temp1, mDists.Width());
-        // if (temp1 <= mDists.Width()) {
-        //     mDists.Width() = temp1;
-        // }
-        for (int i = 0; i < max; i++) {
+    if (f2 >= f3) {
+        return false;
+    } else {
+        node.err = f1;
+        int distsWidth = mDists.Width();
+        int i1 = (f3 - mAStart) * (float)mSamplesPerBeat;
+        int i2 = (f2 - mAStart) * (float)mSamplesPerBeat;
+        int i = Max(i2, 0);
+        distsWidth = Max(i1, distsWidth);
+        for (; i < distsWidth; i++) {
+            float samps = mSamplesPerBeat;
+            int j = mDists.Height();
+            int minus = j - 1;
+            if (minus >= 0) {
+                for (; j != 0; j--, minus--) {
+                    if (MinEq(node.err, mDists(i, minus))) {
+                        node.a = (float)i / samps + mAStart;
+                        node.b = (float)minus / samps + mBStart;
+                    }
+                }
+            }
         }
+        return node.err < f1;
     }
-    return false;
 }
 
 void ClipDistMap::FindNodes(float f1, float f2, float f3) {
     mNodes.clear();
     mLastMinErr = f1;
 
-    float f4 = f2 * 0.45f;
-    if (f2 == 0.0f) {
-        f4 = kHugeFloat;
-        f3 = f4;
-    } else if (f3 == 0.0f) {
-        f3 = f4;
+    float f7 = f2 * 0.45f;
+    if (f2 == 0) {
+        f7 = kHugeFloat;
+        f3 = f7;
+    } else if (f3 == 0) {
+        f3 = f2;
     }
 
-    FindBestNodeRecurse(f1, f4, (f4 * 2.0f - f2), mAStart, mAEnd);
-
+    FindBestNodeRecurse(f1, f7, -(f7 * 2.0f - f2), mAStart, mAEnd);
     std::sort(mNodes.begin(), mNodes.end(), DistMapNodeSort());
-
-    if (!mNodes.empty() && f3 > 0.0f) {
-        float lastNodeDist = mAEnd - mNodes.back().a;
-        if (lastNodeDist > f3) {
-            ClipDistMap::Node node;
+    if (!mNodes.empty() && f3 > 0) {
+        if (mAEnd - mNodes.back().a > f3) {
+            Node node;
             if (FindBestNode(f1, mAEnd - f3, mAEnd, node)) {
                 mNodes.push_back(node);
                 std::sort(mNodes.begin(), mNodes.end(), DistMapNodeSort());
             }
         }
     }
-
-    int limit = mNodes.size() - 1;
-    if (limit > 1) {
-        for (int i = 1; i < limit;) {
-            float dist = mNodes[i + 1].a - mNodes[i].a;
-            if (dist < f2) {
-                mNodes.erase(mNodes.begin() + (i + 1));
-                i--;
-            }
-            i++;
-            limit = mNodes.size() - 1;
+    for (int i = 1; i < (int)mNodes.size() - 1; i++) {
+        if (mNodes[i + 1].a - mNodes[i - 1].a < f2) {
+            mNodes.erase(mNodes.begin() + i);
+            i--;
         }
     }
 }
@@ -191,4 +192,57 @@ void ClipDistMap::DrawDot(float x, float y, float f3, float f4, Hmx::Color const
     rect.x = (f3 - mAStart) * scale * 2.0f + (x - 1.0f);
     rect.y = ((f4 - mBStart) * scale - (float)(mDists.Height() - 1)) * 2.0f + y + 1.0f;
     TheRnd.DrawRect(rect, color, nullptr, nullptr, nullptr);
+}
+
+bool ClipDistMap::LocalMin(int i, int j) {
+    float data = mDists(i, j);
+    if (data == kHugeFloat) {
+        return false;
+    } else {
+        if (mBeatAlign != 0 && BeatAligned(i, j)) {
+            if (i - 1 >= 0 && j - 1 >= 0 && mDists(i - 1, j - 1) < data) {
+                return false;
+            }
+            if (i + 1 < mDists.Width() && j + 1 < mDists.Height()
+                && mDists(i + 1, j + 1) < data) {
+                return false;
+            }
+            return true;
+        }
+
+        for (int n = i - 1; n < i + 2; n++) {
+            for (int i9 = j - 1; i9 < j + 2; i9++) {
+                if (n != i || i9 != j) {
+                    if (n >= 0 && n < mDists.Width() && i9 >= 0 && i9 < mDists.Height()) {
+                        float curData = mDists(n, i9);
+                        if (curData != kHugeFloat && curData < data) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
+
+void ClipDistMap::FindBestNodeRecurse(
+    float f1, float minDist, float f3, float f4, float f5
+) {
+    MILO_ASSERT(minDist > 0, 0x26C);
+    if ((f5 - f4) > f3) {
+        float f10 = Max(f5, minDist + f4 + f3);
+        float f9 = Min(f4, (f5 - f3) - minDist);
+        Node n;
+        if (FindBestNode(f1, f9, f10, n)) {
+            for (int i = 0; i < mNodes.size(); i++) {
+                if (mNodes[i].a == n.a) {
+                    return;
+                }
+            }
+            mNodes.push_back(n);
+            FindBestNodeRecurse(f1, minDist, f3, n.a + minDist, f5);
+            FindBestNodeRecurse(f1, minDist, f3, f4, n.a - minDist);
+        }
+    }
 }
